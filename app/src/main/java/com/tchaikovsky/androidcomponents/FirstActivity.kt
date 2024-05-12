8fe2848bb1092ae93d8a3f0c2643d8def6f3f31a
anton-tchaikovsky <106504887+anton-tchaikovsky@users.noreply.github.com>
2024-05-12 09:42:11 +0300

    Result API for Activity with ActivityResultContracts.StartActivityForResult().

--- app/src/main/java/com/tchaikovsky/androidcomponents/FirstActivity.kt
+++ app/src/main/java/com/tchaikovsky/androidcomponents/FirstActivity.kt
@@ -1,48 +1,53 @@
 package com.tchaikovsky.androidcomponents
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 
 class FirstActivity : AppCompatActivity() {
 
-    private val activityResultContract: ActivityResultContract<String, String?> =
-        object : ActivityResultContract<String, String?>() {
-            override fun createIntent(context: Context, input: String): Intent =
-                Intent(context, SecondActivity::class.java).putExtra(
-                    INPUT_KEY_FOR_ACTIVITY_RESULT,
-                    input
-                )
-
-            override fun parseResult(resultCode: Int, intent: Intent?): String? =
-                if (resultCode != Activity.RESULT_OK) null
-                else intent?.getStringExtra(OUTPUT_KEY_FOR_ACTIVITY_RESULT)
-
-            override fun getSynchronousResult(
-                context: Context,
-                input: String
-            ): SynchronousResult<String?>? =
-                if (input.isNotBlank()) null
-                else SynchronousResult(getString(R.string.default_result))
-
-        }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
-        val activityResultLauncher = registerForActivityResult(activityResultContract) {
-            Toast.makeText(this, if (it.isNullOrBlank()) getString(R.string.default_result) else it, Toast.LENGTH_SHORT)
-                .show()
-        }
+        val activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+            {
+                if (it.resultCode != Activity.RESULT_OK)
+                    Toast.makeText(this, getString(R.string.default_result), Toast.LENGTH_SHORT)
+                        .show()
+                else {
+                    val data = it.data?.getStringExtra(OUTPUT_KEY_FOR_ACTIVITY_RESULT)
+                    Toast.makeText(
+                        this,
+                        if (data.isNullOrBlank()) getString(R.string.default_result) else data,
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            }
 
         findViewById<AppCompatButton>(R.id.first_ok_button).setOnClickListener {
-            activityResultLauncher.launch(findViewById<EditText>(R.id.first_activity_edit_text).text.toString())
+            with(findViewById<EditText>(R.id.first_activity_edit_text).text.toString()) {
+                if (this.isBlank())
+                    Toast.makeText(
+                        this@FirstActivity,
+                        getString(R.string.default_result),
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                else
+                    activityResultLauncher.launch(
+                        Intent(this@FirstActivity, SecondActivity::class.java).putExtra(
+                            INPUT_KEY_FOR_ACTIVITY_RESULT,
+                            this
+                        )
+                    )
+            }
         }
     }
 
